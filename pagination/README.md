743fda3962afe2a84ad42c23a30b82c7650f29fc
likhitha <likhitha.d01@gmail.com>
2020-09-04 20:05:33 +0530

    updated pagination

---
+++ pagination/README.md
@@ -0,0 +1,18 @@
+# Pagination
+
+Scraping of the product URLs in the search page using selenium.
+
+## Dynamic pagination
+
+The products will keep updating while scrolling down.
+
+1. Ajio
+2. Bewakoof
+3. Limeroad
+
+## Static pagination
+
+The products in each page and fixed, and the static page can be scrapped.
+
+1. Myntra
+2. Flipkart.
---
+++ pagination/static/myntra.py
@@ -0,0 +1,50 @@
+import json
+from lxml import html
+import re
+from selenium import webdriver
+import time
+
+url = 'https://www.myntra.com/women-jeans-jeggings'
+
+sub_url = '?p='
+options = webdriver.FirefoxOptions()
+options.add_argument('--headless')
+driver = webdriver.Firefox(options=options,
+			executable_path='/home/likhitha/.firefox/geckodriver')
+driver.get(url)
+driver.maximize_window()
+
+total_count = driver.find_element_by_css_selector('.title-count').get_attribute('innerText')
+total_count = int(re.sub(r'[^\d]','',total_count))
+print(total_count)
+
+num_pages = int(total_count/50)+2
+print(num_pages)
+
+SCROLL_TIME = 5
+count=1
+
+urls_list=[]
+
+category = url.strip().split('/')[-1]
+
+f = open('myntra_'+category+'.txt','w')
+
+for num1 in range(1,num_pages):
+	driver.get(url+sub_url+str(num1))
+	driver.execute_script('window.scrollTo(0,5000)')
+	time.sleep(SCROLL_TIME)
+
+	for num2 in range(1,51):
+		_url_ = driver.find_element_by_css_selector(
+			'li.product-base:nth-child('+str(num2)+') > a:nth-child(2)').get_attribute('href')
+		print(count, _url_)
+		count+=1
+		urls_list.append(_url_)
+		f.write(_url_+'\n')
+
+f.close()
+if len(urls_list) == total_count:
+	print("All URLs are retrieved")
+else:
+	print("Some URLs are missing")
